e3625a1919c5de25f860c0e040f2ae50287b90d0
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-06-07 20:51:37 -0500

    adds xstate

--- components/TrafficLight.js
+++ components/TrafficLight.js
@@ -1,12 +1,41 @@
-import Light from '../components/Light'
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+
+import Light from "../components/Light";
 import styles from "../styles/Home.module.css";
 
+const lightMachine = createMachine({
+  // Machine identifier
+  id: "light",
+
+  // Initial state
+  initial: "red",
+  // State definitions
+  states: {
+    green: {
+      on: {
+        CHANGE_TO_YELLOW: "yellow",
+      },
+    },
+    yellow: {
+      on: {
+        CHANGE_TO_RED: "red",
+      },
+    },
+    red: {
+      on: {
+        CHANGE_TO_GREEN: "green",
+      },
+    },
+  },
+});
+
 export default function TrafficLight() {
-    return (
-        <div className={styles.lightContainer}>
-            <Light color="red" />
-            <Light color="yellow"/>
-            <Light color="green" />
-        </div>
-    )
+  return (
+    <div className={styles.lightContainer}>
+      <Light color="red" />
+      <Light color="yellow" />
+      <Light color="green" />
+    </div>
+  );
 }
